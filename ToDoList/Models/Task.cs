d5db0f0f5072919bd4f0582ae18ed4e8fbf69453
jaquesilva <jaqueline.natario@gmail.com>
2024-05-10 09:38:58 -0300

    Feat: Setup project.

---
+++ ToDoList/Models/Task.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Models
+{
+    public class Task
+    {
+        public int TaskId { get; set; }
+
+        [Display(Name = "Task Name")]
+        public string? TaskName { get; set; }
+
+        public TaskStatus Status { get; set; }
+
+        [Display(Name = "Created Date")]
+        public DateTime CreatedDate { get; set; }
+
+        [Display(Name = "ModifiedDate")]
+        public DateTime ModifiedDate { get; set;}
+
+        public enum TaskStatus
+        {
+            ToDo,
+            Done
+        }
+
+
+
+
+
+    }
+}
