069dbae8c12557af0431c9a5e8cc9ee9859b03fd
jaquesilva <jaqueline.natario@gmail.com>
2024-05-10 14:00:20 -0300

    Feat: create file 'Create.cs' in folder 'TaskItem'

--- ToDoList/Controllers/TaskItemController.cs
+++ ToDoList/Controllers/TaskItemController.cs
@@ -9,12 +9,30 @@ namespace ToDoList.Controllers
         // Estou utilizando uma lista para representar o BD.
         private static List<TaskItem> _tasks = new List<TaskItem>()
         {
-            new TaskItem { TaskId= 1, TaskName="Estudar", Status=TaskItem.TaskStatus.ToDo, TaskCompletedDate=DateTime.Now.AddDays(1) },
-            new TaskItem { TaskId= 2, TaskName="Trabalhar", Status=TaskItem.TaskStatus.ToDo, TaskCompletedDate=DateTime.Now.AddDays(2) }
+            new TaskItem { TaskId= 1, TaskName="Estudar", Status=TaskItem.TaskStatus.Done, TaskCompletedDate=DateTime.Now.AddDays(1) },
+            new TaskItem { TaskId= 2, TaskName="Trabalhar", Status=TaskItem.TaskStatus.Done, TaskCompletedDate=DateTime.Now.AddDays(2) }
         };
         public IActionResult Index()
         {
             return View(_tasks);
+        }
+
+        [HttpGet] //anotação de PEGAR
+        public IActionResult Create()
+        {  //chama o form de cadastro
+            return View();
+        }
+
+        [HttpPost] //Anotação de ENVIAR
+        public IActionResult Create(TaskItem task)
+        { //recebe os dados do form    
+
+            if (ModelState.IsValid)
+            {
+                task.TaskId = _tasks.Count > 0 ? _tasks.Max(c => c.TaskId) + 1 : 1;
+                _tasks.Add(task);
+            }
+            return RedirectToAction("Index");
         }
     }
 }
